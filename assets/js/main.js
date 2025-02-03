e8f2cb9e8a9cf470078bac83ebc6d51a9584ee3e
Brandon Claveria <48102030+swiftrax@users.noreply.github.com>
2025-02-02 16:28:39 -0800

    add darkmode

--- assets/js/main.js
+++ assets/js/main.js
@@ -113,5 +113,11 @@
 				});
 
 			});
-
+			document.getElementById('darkModeToggle').addEventListener('change', function() {
+				if (this.checked) {
+					document.body.classList.add('dark-mode');
+				} else {
+					document.body.classList.remove('dark-mode');
+				}
+			});
 })(jQuery);
